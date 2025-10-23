694225d02a30f596cfc8716929b334ef03072036
Your Name <niyonagizerachel10@email.com>
2025-10-23 12:44:12 +0200

    ready to push

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
-import React from 'react'
-import Approved from './approved/page'
-import Executive from './executive/page'
-import Pendingapprovals from './pending approvals/page'
-import Profile from './profile/page'
-import Reports from './reports/page'
-import Signout from './signout/page'
+import React from "react";
+
+import Pendingapprovals from "./pending approvals/page";
 
 const page = () => {
   return (
     <div>
-      <Approved />
-      <Executive />
       <Pendingapprovals />
-      <Profile/>
-      <Reports/>
-      <Signout/>
     </div>
   );
-}
+};
 
-export default page
+export default page;
